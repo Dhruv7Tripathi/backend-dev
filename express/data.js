083b1b7e9b184e9d5bae1ea6db22ebb64a64f25e
Dhruv7Tripathi <tripathidhruv366@gmail.com>
2026-01-23 14:56:31 +0530

    Add package.json with express dependency

---
+++ express/data.js
@@ -0,0 +1,17 @@
+let data = [{
+  id: 1,
+  name: "Express.js",
+  type: "Web Framework"
+}, {
+  id: 2,
+  name: "Node.js",
+  type: "Runtime Environment"
+}, {
+  id: 3,
+  name: "JavaScript",
+  type: "Programming Language"
+
+}
+]
+
+module.exports = data;
