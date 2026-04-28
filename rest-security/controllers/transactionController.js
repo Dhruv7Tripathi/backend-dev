ad971f6109b7d863443f68d3780c90b3a3a8cc81
Dhruv7Tripathi <tripathidhruv366@gmail.com>
2026-04-28 23:13:35 +0530

    Solve assignment rest security

---
+++ rest-security/controllers/transactionController.js
@@ -0,0 +1,19 @@
+const Transaction = require("../models/Transaction");
+
+exports.createTransaction = async (req, res) => {
+    const { amount, description } = req.body;
+
+    if (amount <= 0) return res.status(400).json({ msg: "Invalid amount" });
+
+    if (amount > 1000) {
+        return res.status(403).json({ msg: "2FA required" });
+    }
+
+    const txn = await Transaction.create({
+        userId: req.user.id,
+        amount,
+        description
+    });
+
+    res.json(txn);
+};
